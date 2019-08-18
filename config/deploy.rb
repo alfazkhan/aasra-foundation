6436e3bc4f592c4363c144dd1d08acca958842b8
Alfaz <khanalfaaz14@gmail.com>
2019-08-18 14:19:14 +0530

    cap task #1

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,29 +11,7 @@ set :repo_url, "https://github.com/alfazkhan/aasra-foundation.git"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-namespace :deploy do
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env};bundle exec rails db:reset RAILS_ENV=#{rails_env}"
-  end
 
-  desc "Remove all but the last release"
-  task :cleanup_all do
-    set :keep_releases, 1
-    invoke "deploy:cleanup"
-  end
-
-  desc "Database Reset"
-  task :db_reset do
-    run "cd #{current_path}; bundle exec rails db:reset RAILS_ENV=#{rails_env}"
-  end
-
-  desc "maintenance mode"
-  task :db_main do
-    run "cd #{current_path}; rake maintenance:start"
-  end
-
-end
 
 
 
